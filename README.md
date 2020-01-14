f49b1d131a5f8b688d2b7ff3e38857dc4ab502af
cindyweiss <weiss_cindy@yahoo.com>
2020-01-14 12:29:22 -0800

    finished first feature

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # lab-06-back-end2
+
+Number and name of feature: _____________setting up repo___________________
+
+Estimate of time needed to complete: ___1hr__
+
+Start time: __11:00___
+
+Finish time: ____12:00_
+
+Actual time needed to complete: _____1hr
+
+
+
+Number and name of feature: _____________creating a location query___________________
+
+Estimate of time needed to complete: ___1hr__
+
+Start time: __12:00___
+
+Finish time: _____
+
+Actual time needed to complete: _____
--- server.js
+++ server.js
@@ -3,8 +3,31 @@
 const express = require('express');
 require('dotenv').config();
 
+const cors = require('cors');
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//the police
+app.use(cors());
+
+
+//routes
+app.get('/location', (request, response) => {
+  let city = request.query.city;
+  const geoData = require('./data/geo.json');
+
+  let location = {
+    search_query: city,
+    formatted_query: geoData[0].display_name,
+    latitude: geoData[0].lat,
+    longitude: geoData[0].lon
+  }
+  response.status(200).send(location);
+})
+
+
 
 //turning it on
 app.listen(PORT, () => {
